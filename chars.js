c22993a7c2c7e44ce265f690d562f3fe8b35fbb1
John Louis Swaine <jlswaine@me.com>
2013-01-10 14:00:08 +0000

    Fixed n00b errors

--- chars.js
+++ chars.js
@@ -1,11 +1,11 @@
-var characterArray = [Mal, Zoe, Wash, River, Jane, Simon, Kaylee];
+var characterArray = ["Mal", "Zoe", "Wash", "River", "Jane", "Simon", "Kaylee"];
 
 exports.chars = function(req, res) {
-  app.send('Responsy');
-}
+  res.send('Responsy');
+};
 
 exports.char = function(req, res) {
   var charname = characterArray[req.params.id];
   res.send(charname);
-}
+};
 
