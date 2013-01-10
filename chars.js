b16bdd18cd5ebeea31e92c6cb701383c353600d4
johnlouis-swaine-axomic <johnlouis.swaine@axomic.com>
2013-01-10 11:12:04 +0000

    Update chars.js

--- chars.js
+++ chars.js
@@ -1,3 +1,12 @@
+
+var characterArray = [Mal, Zoe, Wash, River, Jane, Simon, Kaylee];
+
 exports.chars = function(req, res) {
   app.send('Responsy');
 }
+
+exports.char = function(req, res) {
+  var charname = characterArray[req.params.id];
+  app.send(charname);
+}
+
